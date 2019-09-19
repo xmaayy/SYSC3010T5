39e24e6f08a9996162fd0bfd5651f2ca1f278f86
Xander May <xandermay1@gmail.com>
2019-09-19 16:27:36 -0400

    Java code completed

--- Lab2/UDPReceiver.java
+++ Lab2/UDPReceiver.java
@@ -17,17 +17,27 @@ public class UDPReceiver {
 	      {
 	         // Convert the argument to ensure that is it valid
 	         int port = Integer.parseInt( args[0] ) ;
+	         
+	         // Make the sender socket
+	         DatagramSocket send_socket = new DatagramSocket() ;
 
 	         // Construct the socket
 	         DatagramSocket socket = new DatagramSocket( port ) ;
 
 	         for( ;; )
 	         {
-		        System.out.println( "Receiving on port " + port ) ;
-		        DatagramPacket packet = new DatagramPacket( new byte[PACKETSIZE], PACKETSIZE ) ;
-	            socket.receive( packet ) ;
-
-	            System.out.println( packet.getAddress() + " " + packet.getPort() + ": " + new String(packet.getData()).trim() ) ;
+				 System.out.println( "Receiving on port " + port ) ;
+				 DatagramPacket packet = new DatagramPacket( new byte[PACKETSIZE], PACKETSIZE ) ;
+				 socket.receive( packet ) ;
+				 
+				 String message = new String(packet.getData()).trim() ;
+				 System.out.println( packet.getAddress() + " " + packet.getPort() + ": " + message ) ;
+				 
+				 System.out.println("Replying ... ");
+				 message = "ACK: " + message ;
+				 byte [] data = message.getBytes() ;
+				 packet = new DatagramPacket( data, data.length, packet.getAddress(), port+1 ) ;
+				 send_socket.send( packet ) ;
 	        }  
 	     }
 	     catch( Exception e )
--- Lab2/UDPSender.java
+++ Lab2/UDPSender.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 
 public class UDPSender {
 
+	private final static int PACKETSIZE = 100 ;
+
 	public static void main(String[] args) 
    {
 	      // Check the arguments
-	      if( args.length != 2 )
+	      if( args.length != 3 )
 	      {
 	         System.out.println( "usage: java UDPSender host port" ) ;
 	         return ;
@@ -17,19 +19,31 @@ public class UDPSender {
 	         // Convert the arguments first, to ensure that they are valid
 	         InetAddress host = InetAddress.getByName( args[0] ) ;
 	         int port         = Integer.parseInt( args[1] ) ;
+	         int repeat       	= Integer.parseInt( args[2] ) ;
 	         socket = new DatagramSocket() ;
+	         
+	         // Construct the socket
+	         DatagramSocket recv_socket = new DatagramSocket( port+1 ) ;
      
 	         Scanner in;
 	         in = new Scanner (System.in);
 	         String message = null;
-	         while (true)
-	         {
-	        		 System.out.println("Enter text to be sent, ENTER to quit ");
-	        		 message = in.nextLine();
-	        		 if (message.length()==0) break;
-	        		 byte [] data = message.getBytes() ;
-	        		 DatagramPacket packet = new DatagramPacket( data, data.length, host, port ) ;
-	        		 socket.send( packet ) ;
+			 System.out.println("Enter text to be sent, ENTER to quit ");
+			 message = in.nextLine();
+			 if (message.length()!=0){
+				 for (int i = 0; i < repeat; i++)
+				 {
+					 String new_message = message + (i+1) ;
+					 byte [] data = new_message.getBytes() ;
+					 DatagramPacket packet = new DatagramPacket( data, data.length, host, port ) ;
+					 socket.send( packet ) ;
+					 
+					 System.out.println("Waiting for reply ... ");				 
+					 packet = new DatagramPacket( new byte[PACKETSIZE], PACKETSIZE ) ;
+					 recv_socket.receive( packet ) ;
+
+					 System.out.println( packet.getAddress() + " " + packet.getPort() + ": " + new String(packet.getData()).trim() ) ;
+				 }
 	         } 
 	         System.out.println ("Closing down");
 	      }
