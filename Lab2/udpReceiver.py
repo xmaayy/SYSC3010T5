c249e6b9bcbcf28a6ff0c220b6a45f1b94d66d98
Xander May <xandermay1@gmail.com>
2019-09-19 15:12:16 -0400

    Exercise 1 Complete

--- Lab2/udpReceiver.py
+++ Lab2/udpReceiver.py
@@ -5,6 +5,7 @@ import socket, sys, time
 textport = sys.argv[1]
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
 port = int(textport)
 server_address = ('localhost', port)
 s.bind(server_address)
@@ -17,5 +18,7 @@ while True:
     if not len(buf):
         break
     print ("Received %s bytes from %s %s: " % (len(buf), address, buf ))
+    # Send back data
+    s.sendto("ACK:{buf}".format(buf=buf).encode('utf-8'), address)
 
 s.shutdown(1)
--- Lab2/udpSender.py
+++ Lab2/udpSender.py
@@ -4,18 +4,23 @@ import socket, sys, time
 
 host = sys.argv[1]
 textport = sys.argv[2]
+repeat = int(sys.argv[3])
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 port = int(textport)
 server_address = (host, port)
 
-while 1:
-    print ("Enter data to transmit: ENTER to quit")
-    data = sys.stdin.readline().strip()
-    if not len(data):
-        break
-#    s.sendall(data.encode('utf-8'))
-    s.sendto(data.encode('utf-8'), server_address)
+print ("Enter data to transmit: ENTER to quit")
+msg = sys.stdin.readline().strip()
+if len(msg) > 0:
+    # Repeat specified number of times
+    for i in range(repeat):
+        data = msg + str(i)
+        s.sendto(data.encode('utf-8'), server_address)
 
-s.shutdown(1)
+        buf, address = s.recvfrom(port)
+        if not len(buf):
+            break
+        print ("Received %s bytes from %s %s: " % (len(buf), address, buf ))
 
+#s.shutdown(1)
